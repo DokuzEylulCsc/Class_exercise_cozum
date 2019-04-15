15648ae51a5c2ff0f9d9482a3f6d925a6f9413ca
Alican <alican.dogan@deu.edu.tr>
2019-04-15 08:54:22 +0300

    cc

---
+++ class_exercise/class_exercise/employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace class_exercise
+{
+    public abstract class employee
+    {
+        string f_name;
+        string l_name;
+        int ssn;
+        public void Employee(string a,string b, int c)
+        {
+            f_name = a;
+            l_name = b;
+            ssn = c;
+        }
+        public abstract double getSalary();
+        public override string ToString()
+        {
+           string x = f_name + " " + l_name + "\n" + "Social Security Number: " + ssn;
+            return x;
+        }
+    }
+}
---
+++ class_exercise/class_exercise/salaried_employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace class_exercise
+{
+    public class salaried_employee : employee
+    {
+        double weekly_salary;
+        public void Salaried_employee(double x)
+        {
+            weekly_salary = x;
+        }
+        public override double getSalary()
+        {
+            return weekly_salary;
+        }
+        public override string ToString()
+        {
+            string x = "Salaried Employee: " + base.ToString() + "\n" + "Weekly Salary: " + getSalary();
+            return x;
+        }
+    }
+}
